3cd816aae83fb0e70cf778f0e3605b768e90b851
khadija <khadijabarakzai97@gmail.com>
2026-08-15 16:19:05 -0400

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/navbar'
 import Home from './components/Home'
 import Catering from './components/Catering'
+import Menu from './components/Menu'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
@@ -17,6 +18,7 @@ function App() {
        <Route path="/" element={<Home />} />
        <Route path="/home" element={<Home />} />
        <Route path="/catering" element={<Catering />} />
+       <Route path="/menu" element={<Menu />} />
        <Route path="/contact" element={<Contact />} />
      </Routes>
      <Footer />
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import './Menu.css'
+
+function Menu() {
+  return (
+    <section className="menu">
+      <h1 className="menu-heading">Menu</h1>
+      <p>Our full menu is coming soon!</p>
+    </section>
+  )
+}
+
+export default Menu
